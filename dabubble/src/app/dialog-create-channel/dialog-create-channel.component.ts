3cb0e1f5e7f2b78a4a6a8e53f59a9471ddd63426
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-12 21:31:24 +0100

    3th commit, created some components

---
+++ dabubble/src/app/dialog-create-channel/dialog-create-channel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-create-channel',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-create-channel.component.html',
+  styleUrl: './dialog-create-channel.component.scss'
+})
+export class DialogCreateChannelComponent {
+
+}
