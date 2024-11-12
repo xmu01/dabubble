80172b180e45c61ccf4d1fc605e4814220428006
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-12 21:51:18 +0100

    5th commit, set-new-password comp

---
+++ dabubble/src/app/set-new-password/set-new-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-new-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-new-password.component.html',
+  styleUrl: './set-new-password.component.scss'
+})
+export class SetNewPasswordComponent {
+
+}
