fca15891f5ad556a5b381b02be62dc99a0885993
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-13 20:34:57 +0100

    added arrow down for user in header

--- dabubble/src/app/header/header.component.ts
+++ dabubble/src/app/header/header.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatIconModule,FormsModule, MatFormFieldModule,
+    MatExpansionModule, MatDialogModule, 
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
