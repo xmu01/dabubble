3b2cd574d6d760f83079f15ab2eeef0d6bd667b4
Pao040883 <patrick@offermanns.online>
2024-11-13 09:47:43 +0100

    Adding content to sidenav

--- dabubble/src/app/sidenav/sidenav.component.ts
+++ dabubble/src/app/sidenav/sidenav.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatCardModule, MatSidenavModule, MatButtonModule],
+  imports: [MatCardModule, MatSidenavModule, MatButtonModule, MatExpansionModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-  showFiller = false;
+  panelOpenState = true;
 }
