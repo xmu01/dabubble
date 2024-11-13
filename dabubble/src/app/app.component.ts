1e8bb9496237ccb23e72b0b036d05f7ae8d3a26d
Pao040883 <patrick@offermanns.online>
2024-11-13 08:43:41 +0100

    Add structure for app compnonent, font, img, basic sidenav elements

--- dabubble/src/app/app.component.ts
+++ dabubble/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SidenavComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- dabubble/src/app/sidenav/sidenav.component.ts
+++ dabubble/src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatSidenavModule, MatButtonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-
+  showFiller = false;
 }
