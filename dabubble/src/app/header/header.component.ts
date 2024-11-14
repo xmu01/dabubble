538290b6201cea8a4bdb5090c7c45dd4759bffa5
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-14 22:18:50 +0100

    added menu for profile in header

--- dabubble/src/app/header/header.component.ts
+++ dabubble/src/app/header/header.component.ts
@@ -6,11 +6,14 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDialogModule} from '@angular/material/dialog';
 import { ViewEncapsulation } from '@angular/core'; // Um Angular Mat-Styles zu überschreiben
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatInputModule, MatIconModule,FormsModule, MatFormFieldModule,
-    MatExpansionModule, MatDialogModule, 
+    MatExpansionModule, MatDialogModule, MatMenuModule, MatButtonModule,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   //encapsulation: ViewEncapsulation.None, //Erstmal draußen, da es Probleme im Header gemacht hat (kaputtes Flex)
