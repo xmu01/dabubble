1a89de785ee5c13bd2c280d77bbde2975c7fb171
Pao040883 <patrick@offermanns.online>
2024-11-14 21:40:45 +0100

    Update message element

--- dabubble/src/app/main-content/main-content.component.ts
+++ dabubble/src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { MatCardModule } from '@angular/material/card';
 import { ThreadComponent } from '../thread/thread.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [MatCardModule, ThreadComponent, MatIconModule, MatButtonModule],
+  imports: [MatCardModule, ThreadComponent, MatIconModule, MatButtonModule, MatInputModule],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
