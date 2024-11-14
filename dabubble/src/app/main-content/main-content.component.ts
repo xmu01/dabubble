00a5cbfb183c7f87792c608e5ddf8c23c790f97d
Pao040883 <patrick@offermanns.online>
2024-11-14 10:39:34 +0100

    Header and title of direct messages

--- dabubble/src/app/main-content/main-content.component.ts
+++ dabubble/src/app/main-content/main-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ThreadComponent } from '../thread/thread.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [MatCardModule, ThreadComponent],
+  imports: [MatCardModule, ThreadComponent, MatIconModule, MatButtonModule],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
