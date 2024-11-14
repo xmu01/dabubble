5b151a3c53f96818fd90affb6d751b9cefe40a44
Pao040883 <patrick@offermanns.online>
2024-11-14 22:04:13 +0100

    Update message element

--- dabubble/src/app/main-content/main-content.component.ts
+++ dabubble/src/app/main-content/main-content.component.ts
@@ -4,14 +4,20 @@ import { ThreadComponent } from '../thread/thread.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [MatCardModule, ThreadComponent, MatIconModule, MatButtonModule, MatInputModule],
+  imports: [MatCardModule, ThreadComponent, MatIconModule, MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
+  message: string = '';
 
+  getMessage() {
+    console.log(this.message);
+    this.message = '';
+  }
 }
