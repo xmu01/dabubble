60bbb2c40f13e51f323cf76de1988f79b5a479f0
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-17 22:11:39 +0100

    imported remaining components to app.component.ts

--- dabubble/src/app/app.component.ts
+++ dabubble/src/app/app.component.ts
@@ -4,11 +4,20 @@ import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { LoginComponent } from './login/login.component';
+import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidenavComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, SidenavComponent, HeaderComponent, LoginComponent, ChooseAvatarComponent, EditProfileComponent,
+  ForgotPasswordComponent, PrivacyPolicyComponent, LegalNoticeComponent, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
