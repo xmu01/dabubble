3a6a9538d479ed4c28f945e5ca2bb114f44449d9
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-24 21:59:33 +0100

    starting to style the new sign up component

--- dabubble/src/app/signup/signup.component.ts
+++ dabubble/src/app/signup/signup.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,  
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 
+  continueSignUp() {
+    
+  }
 }
