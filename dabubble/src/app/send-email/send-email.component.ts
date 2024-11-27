b0f137ed0871519a0bb99df7ffe33290117f31a9
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-27 22:39:37 +0100

    imported modules for those 2 components

--- dabubble/src/app/send-email/send-email.component.ts
+++ dabubble/src/app/send-email/send-email.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-send-email',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './send-email.component.html',
   styleUrl: './send-email.component.scss'
 })
 export class SendEmailComponent {
 
+  continueSignUp2() {
+    
+  }
 }
