4b936c474c08093e5720f4ed419a2b0e37630d2c
Muhammed Ahmed Yazea <muhammedahmedyazea@gmail.com>
2024-11-27 19:19:54 +0100

    deleted old set-password component and created 2 new send-email and reset-password components

--- dabubble/src/app/app.routes.ts
+++ dabubble/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SignupComponent } from './signup/signup.component';
+import { SendEmailComponent } from './send-email/send-email.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 export const routes: Routes = [
 
@@ -14,7 +15,8 @@ export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'choose-avatar', component: ChooseAvatarComponent },
     { path: 'edit-profile', component: EditProfileComponent },
-    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'send-email', component: SendEmailComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: 'legal-notice', component: LegalNoticeComponent },
     { path: 'sign-up', component: SignupComponent },
--- dabubble/src/app/reset-password/reset-password.component.spec.ts
+++ dabubble/src/app/reset-password/reset-password.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SetNewPasswordComponent } from './set-new-password.component';
+import { ResetPasswordComponent } from './reset-password.component';
 
-describe('SetNewPasswordComponent', () => {
-  let component: SetNewPasswordComponent;
-  let fixture: ComponentFixture<SetNewPasswordComponent>;
+describe('ResetPasswordComponent', () => {
+  let component: ResetPasswordComponent;
+  let fixture: ComponentFixture<ResetPasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SetNewPasswordComponent]
+      imports: [ResetPasswordComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SetNewPasswordComponent);
+    fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
