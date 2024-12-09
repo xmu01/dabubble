381a346f4606e1b36aec64ec9540d821917bd53f
Pao040883 <patrick@offermanns.online>
2024-12-09 14:27:16 +0100

    Add function to edit message

---
+++ dabubble/src/app/main-content/channel-messages/channel-messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-messages',
+  standalone: true,
+  imports: [],
+  templateUrl: './channel-messages.component.html',
+  styleUrl: './channel-messages.component.scss'
+})
+export class ChannelMessagesComponent {
+
+}
---
+++ dabubble/src/app/main-content/direct-messages/direct-messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direct-messages',
+  standalone: true,
+  imports: [],
+  templateUrl: './direct-messages.component.html',
+  styleUrl: './direct-messages.component.scss'
+})
+export class DirectMessagesComponent {
+
+}
